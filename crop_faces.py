43227cb8c301c1cdead0d45043533e4c4dbb60b4
Vincent COUVERCHEL <vincent@couverchel.eu>
2019-06-08 14:32:56 +0800

    update croping + add MUNIT

--- crop_faces.py
+++ crop_faces.py
@@ -4,7 +4,7 @@ from PIL import Image
 import random
 import os
 
-def crop_faces(landmarks, input, output, confidence, ratio, min_size):
+def crop_faces(landmarks, input, output, output_fail, confidence, ratio, min_size, size):
     with open(landmarks, 'r') as f:
         json_file = f.read()
         landmarks = json.loads(json_file)
@@ -14,7 +14,8 @@ def crop_faces(landmarks, input, output, confidence, ratio, min_size):
             if os.path.exists(fn):
                 img = Image.open(fn)
                 for j, face in enumerate(faces):
-                    if face['score'] >= confidence:
+                    if face['score'] >= confidence or output_fail is not None:
+                        out_folder = output if face['score'] >= confidence else output_fail
                         box = face['bbox']
                         width = box[2] - box[0]
                         height = box[3] - box[1]
@@ -28,7 +29,9 @@ def crop_faces(landmarks, input, output, confidence, ratio, min_size):
                                         box[1] - pad_h - m * side_coef,
                                         box[2] + pad_w + m * side_coef,
                                         box[3] + pad_h + m * side_coef))
-                        save_fn = os.path.join(output, '{}-{}.png'.format(key, j))
+                        if size > 0:
+                            crop = crop.resize((size, size), Image.ANTIALIAS)
+                        save_fn = os.path.join(out_folder, '{}-{}.png'.format(key, j))
                         crop.save(save_fn)
             else:
                 print('skip {}'.format(fn))
@@ -37,12 +40,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--output', help='output folder path')
-    parser.add_argument('--input', help='input folder path')
-    parser.add_argument('--landmarks', help='landmark json path')
-    parser.add_argument('--conf', type=float, default=0.1, help='minimim confidence threshold')
-    parser.add_argument('--ratio', type=float, default=1.2, help='face cutting ratio')
-    parser.add_argument('--min-size', type=int, default=64, help='minimum face size')
+    parser.add_argument('-output', help='output folder path')
+    parser.add_argument('-output-fail', default=None, help='weak likelihood output folder path')
+    parser.add_argument('-input', help='input folder path')
+    parser.add_argument('-landmarks', help='landmark json path')
+    parser.add_argument('-conf', type=float, default=0.1, help='minimum confidence threshold')
+    parser.add_argument('-ratio', type=float, default=1.2, help='face cutting ratio')
+    parser.add_argument('-min-size', type=int, default=64, help='minimum face size')
+    parser.add_argument('-size', type=int, default=-1, help='desired face size')
     args = parser.parse_args()
 
-    crop_faces(args.landmarks, args.input, args.output, args.conf, args.ratio, args.min_size)
+    crop_faces(args.landmarks, args.input, args.output, args.output_fail, args.conf, args.ratio, args.min_size, args.size)
